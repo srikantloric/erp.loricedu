15605ad5597be936ea1da304482591b2794132cb
Srikant Loric <srikantloric@gmail.com>
2025-11-21 01:02:17 +0530

    attendance module

--- src/pages/Attendance/viewAttendanceTabs/OverViewTab.tsx
+++ src/pages/Attendance/viewAttendanceTabs/OverViewTab.tsx
@@ -1,31 +1,85 @@
 import { Grid } from "@mui/joy";
 import { Typography } from "@mui/material";
 import RoundIconCard from "components/Card/RoundIconCard";
+import { useFirebase } from "context/firebaseContext";
+import { collection, collectionGroup, getCountFromServer, query, where } from "firebase/firestore";
 import { Clock, Forbidden2, TickCircle } from "iconsax-react";
+import { useEffect, useState } from "react";
 
 function OverViewTab() {
+
+  const todaysDate = new Date().toLocaleString();
+  const [totalStudents, setTotalStudents] = useState(0);
+  const [attendanceSummary, setAttendanceSummary] = useState({
+    totalPresent: 0,
+    totalAbsent: 0,
+    totalOnLeave: 0,
+  });
+
+  const { db } = useFirebase()
+
+  useEffect(() => {
+    //students count from firestore STUDENTS collection
+    const fetchStudentCount = async () => {
+      try {
+        const studentsRef = collection(db, "STUDENTS");
+        const q = query(studentsRef);
+        const snapshot = await getCountFromServer(q);
+        setTotalStudents(snapshot.data().count);
+      } catch (error) {
+        console.error("Error fetching count:", error);
+      }
+    };
+
+    const fetchAttendanceSummary = async () => {
+      try {
+        const attendanceRef = collectionGroup(db, "MY_ATTENDANCE");
+        // const today = new Date().toISOString().split("T")[0];
+        const today = "2025-11-20";
+        const q = query(
+          attendanceRef,
+          where("date", "==", today),
+
+        );
+        const snapshot = await getCountFromServer(q);
+        const totalPresent = snapshot.data().count;
+        setAttendanceSummary((prev) => ({
+          ...prev,
+          totalPresent: totalPresent,
+        }));
+      } catch (error) {
+        console.error("Error fetching attendance summary:", error);
+      }
+    }
+
+    fetchStudentCount();
+    fetchAttendanceSummary();
+
+  }, [])
+
+
   return (
     <>
       <Typography>Today's Report</Typography>
       <Grid container spacing={2} mt={1}>
         <Grid xs={12} md={4}>
-         
+
           <RoundIconCard
             iconPrimary={TickCircle}
             primary="Total Present"
-            secondary="225"
-            content="22nd April 2024"
+            secondary={"" + attendanceSummary.totalPresent}
+            content={todaysDate}
             color="#1b5e20"
             bgcolor="#c8e6c9"
-            />
-            
+          />
+
         </Grid>
         <Grid xs={12} md={4}>
           <RoundIconCard
             iconPrimary={Clock}
             primary="Total Absent"
-            secondary="25"
-            content="22nd April 2024"
+            secondary={"" + (totalStudents - attendanceSummary.totalPresent)}
+            content={todaysDate}
             color="#b71c1c"
             bgcolor="#ffcdd2"
           />
@@ -34,8 +88,8 @@ function OverViewTab() {
           <RoundIconCard
             iconPrimary={Forbidden2}
             primary="Total On Leave"
-            secondary="15"
-            content="22nd April 2024"
+            secondary={"" + attendanceSummary.totalOnLeave}
+            content={todaysDate}
             color="#01579b"
             bgcolor="#b3e5fc"
           />
